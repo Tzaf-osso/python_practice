a8d0f2438b8d965757480a2d441ff13bc6543f31
Tzafrir <tzafrir4321@gmail.com>
2020-04-30 13:39:42 +0300

    add solution for chapter4

---
+++ tar4.py
@@ -0,0 +1,29 @@
+# ************** Chapter 4 solution *************************
+
+# ***********************************************************
+# i) format - help us use print with different types of variable
+# ***********************************************************
+
+# ***********************************************************
+# ii) raw_input
+# ***********************************************************
+
+# ***********************************************************
+# ii) raw_input
+# ***********************************************************
+
+# ***********************************************************
+# iii)
+# ***********************************************************
+string = "hello"
+string = string[::-1]
+print(string)
+
+# ***********************************************************
+# iv)
+#       a. upper() - Converts a string into upper case
+#       b. count() - Returns the number of times a specified value occurs in a string
+#       a. index() - Searches the string for a specified value and returns the position of where it was found
+#       b. isalpha() - Returns True if all characters in the string are in the alphabet
+#       a. capitalize() - Converts the first character to upper case
+# ***********************************************************
