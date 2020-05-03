4fa057893d3a41dc73952635465751a25bd4cfd2
Tzafrir <tzafrir4321@gmail.com>
2020-05-03 15:06:19 +0300

    add chaptr6 solution

---
+++ tar6.py
@@ -0,0 +1,10 @@
+# ************** Chapter  solution *************************
+
+# ***********************************************************
+# i)
+#       list -  collection of arbitrary objects.mutable. we can change the list and add more items
+#       tuple - collection of arbitrary objects. immutable. we can't change the lists.
+
+# ii) על רשימות ניתן לבצע כמה םעולות עיקריות:
+#    הוספה איבר בסוף הרשימה ,הוצאת איבר מהרשימה,למיין איברים ברשימה ואיחוד מספר איבברים למחרוזת אחת
+# ***********************************************************
